4b52a16ca8d04802ed8a545bd3c6648af54ecd5b
tedkkraft <tedkkraft@gmail.com>
2020-06-19 23:27:47 -0400

    Added question set. Still having styling issues.

---
+++ assets/javascript/scripts.js
@@ -0,0 +1,28 @@
+var questions = [{
+    question: "A JavaScript string is zero or more characters written inside...",
+    answers: ["Brackets", "Parentheses", "Quotes", "Semicolons"],
+    correctAnswer: "Quotes"
+}, {
+    question: "JavaScript arrays are used to store multiple ______ in a single variable.",
+    answers: ["Numbers", "Characters", "Functions", "Values"],
+    correctAnswer: "Values"
+}, {
+    question: "Comments in Javascript start with...",
+    answers: ["//", "/*", "-m", "<--"],
+    correctAnswer: "//"
+}, {
+    question: "When identifying variables in JavaScript with unique names, names can begin with letters, digits, underscores, or a dollar sign.",
+    answers: ["True", "False"],
+    correctAnswer: "False"
+}, {
+    question: "Which of the following examples properly defines a function?",
+    answers: ["function newFunction()", "function = newFunction()", "var = newFunction;", ".newFunction()"],
+    correctAnswer: "function newFunction()"
+}, {
+    question: "The code to be executed by a function is placed inside...",
+    answers: ["Brackets", "Curly brackets", "Quotes", "Parentheses"],
+    correctAnswer: "Curly brackets"
+}, {
+
+
+}];
